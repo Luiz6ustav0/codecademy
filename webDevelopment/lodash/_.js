50c3120de9f84dc7dd4d154eddf8ad93b0de9197
Luiz Almeida <luizorst@hotmail.com>
2020-04-25 00:00:58 -0300

    Implement clamp method

--- webDevelopment/lodash/_.js
+++ webDevelopment/lodash/_.js
@@ -1,4 +1,16 @@
+let _ = {
+    // Takes three arguments and if number is already between the upper 
+    // and lower bound, returns number. If number is smaller then the
+    // lower bound it will  return the lower bound and if number is greater
+    // than the upperBound the method will return the upperBound.
+    clamp(number, lowerBound, upperBound){
+        let clampedValue = Math.max(number, lowerBound);
+        clampedValue = Math.min(clampedValue, upperBound);
+        return clampedValue;
+    }
 
+
+};
 
 
 
