1d104dd63bf976c09bb9de3362b318421fab2912
Luiz Almeida <luizorst@hotmail.com>
2020-05-11 00:57:02 -0300

    Implement renderVenues

--- webDevelopment/wanderlust/public/main.js
+++ webDevelopment/wanderlust/public/main.js
@@ -49,9 +49,16 @@ const getForecast = async () => {
 // Render functions
 const renderVenues = (venues) => {
   $venueDivs.forEach(($venue, index) => {
-    // Add your code here:
+    const venue = venues[index];
+    const venueIcon = venue.categories[0].icon;
+    const venueImgSrc = venueIcon.prefix + 'bg_64' + venueIcon.suffix;
 
-    let venueContent = '';
+    let venueContent = `<h2>${venue.name}</h2>
+                        <img class="venueimage" src="${venueImgSrc}"/>
+                        <h3>Address: </h3>
+                        <p>${venue.location.address}</p>
+                        <p>${venue.location.city}</p>
+                        <p>${venue.location.country}</p>`
     $venue.append(venueContent);
   });
   $destination.append(`<h2>${venues[0].location.city}</h2>`);
@@ -69,7 +76,7 @@ const executeSearch = () => {
   $weatherDiv.empty();
   $destination.empty();
   $container.css("visibility", "visible");
-  getVenues()
+  getVenues().then(venues => renderVenues(venues));
   getForecast()
   return false;
 }
