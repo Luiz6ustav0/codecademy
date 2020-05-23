d5502fe684e7e75bc6b25a32ff495b6039baac5e
Luiz Almeida <luizorst@hotmail.com>
2020-05-23 01:38:13 -0300

    Implement communication with API

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -7,8 +7,19 @@ import {Yelp} from './util/Yelp';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      businesses: [],
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy){
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term, location, sortBy)
+    .then(businesses => {
+      this.setState({businesses: businesses});
+    });
   }
 
   render() {
@@ -16,7 +27,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businessArr}/>
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
