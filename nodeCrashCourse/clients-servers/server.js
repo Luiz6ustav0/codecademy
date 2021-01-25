5537e114379bd8f5bab078aeb367f2c6089ec128
Luiz Almeida <luizorst@hotmail.com>
2021-01-25 01:16:50 -0300

    http module

---
+++ nodeCrashCourse/clients-servers/server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, response) => {
+    console.log(req.url, req.method);
+    
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            response.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            response.statusCode = 200;
+            break;
+        case '/about-me':
+            response.statusCode = 301;
+            response.setHeader('Location', '/about');
+            // response.end();
+            break;
+        default:
+            path += '404.html';
+            response.statusCode = 404;
+            break;
+    }
+
+    // set header content type
+    response.setHeader('Content-Type', 'text/html');
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            response.end();
+        } else {
+            response.end(data);
+        }
+    })
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log("Server running on port 3000");
+});
