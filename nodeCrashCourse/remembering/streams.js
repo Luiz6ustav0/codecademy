17f3f2a7eb24531b91c85cbfc4211103f94fd7dc
Luiz Almeida <luizorst@hotmail.com>
2021-01-25 00:30:40 -0300

    streams

---
+++ nodeCrashCourse/remembering/streams.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const readS = fs.createReadStream('./docs/blog3.txt', {encoding : 'utf-8'});
+const writeS = fs.WriteStream('./docs/blog4.txt');
+
+readS.on('data', (chunk) => {
+    console.log("------New chunk------");
+    console.log(chunk);
+    writeS.write('\nNew chunk!\n');
+    writeS.write(chunk);
+})
