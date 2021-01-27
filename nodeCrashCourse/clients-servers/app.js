bf593c43d15a53996b331825445eff5c7dad4a45
Luiz Almeida <luizorst@hotmail.com>
2021-01-26 23:48:27 -0300

    add new post route

--- nodeCrashCourse/clients-servers/app.js
+++ nodeCrashCourse/clients-servers/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const {login, pw, db} = require('./secrets');
+const { login, pw, db } = require('./secrets');
 
 const Blog = require('./models/blog');
 
@@ -9,14 +9,15 @@ const Blog = require('./models/blog');
 const app = express();
 
 const dbURI = `mongodb+srv://${login}:${pw}@cluster0.l6tbh.mongodb.net/${db}?retryWrites=true&w=majority`;
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then((result) => app.listen(3000))
-    .catch((err) => console.log(err));
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // middleware & static files
 
 app.use(morgan('dev'));
 app.use(express.static('.public'));
+app.use(express.urlencoded({ extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -29,11 +30,21 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All blogs', blogs: result });
-        })
-        .catch((err) => console.log(err));
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'All blogs', blogs: result });
+    })
+    .catch((err) => console.log(err));
+})
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => console.log(err));
 })
 
 app.get('/blogs/create', (req, res) => {
