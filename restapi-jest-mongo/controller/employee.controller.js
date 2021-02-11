b13a927f34e3937e9d02fab595bd95f778100948
Luiz Almeida <luizorst@hotmail.com>
2021-02-10 23:35:10 -0300

    add email and schema validation

--- restapi-jest-mongo/controller/employee.controller.js
+++ restapi-jest-mongo/controller/employee.controller.js
@@ -1,8 +1,26 @@
 const employeeModel = require('../models/employee.model');
+const joi = require('@hapi/joi');
+const bcrypt = require('bcrypt');
+
+const schema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().required().email(),
+    password: joi.string().min(6).max(12).required(),
+    gender: joi.string(),
+    phone: joi.string()
+})
 
 exports.createEmployee = async (req, res, next) => {
     try {
         console.log("Create employee caleed");
+        
+        const joiCheck = await schema.validate(req.body);
+        console.log("Validation result: ", joiCheck);
+        if (joiCheck.error) return res.status(400).json(joiCheck.error);
+
+        const emailExist = await employeeModel.findOne({email: req.body.email});
+        if (emailExist) return res.status(400).json({message: 'The email provided already exists in the database.'});
+
         const newEmployee = await (employeeModel.create(req.body));
         res.status(201).json(newEmployee);
     }
